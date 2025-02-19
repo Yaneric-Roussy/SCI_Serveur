6aea38d05273dda479a5974677a69cfe28a3a303
yaneric <2243398@cegepmontpetit.ca>
2025-02-19 17:50:40 -0500

    PROGRÈS Ajout logique pour JoinMatch

--- sci-depart-main/WebApi/Hubs/MatchHub.cs
+++ sci-depart-main/WebApi/Hubs/MatchHub.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Identity.Client;
 using Super_Cartes_Infinies.Data;
+using Super_Cartes_Infinies.Models.Dtos;
+using Super_Cartes_Infinies.Services;
 
 namespace Super_Cartes_Infinies.Hubs;
 
@@ -9,13 +11,23 @@ namespace Super_Cartes_Infinies.Hubs;
 public class MatchHub : Hub
 {
     ApplicationDbContext _context;
-    public MatchHub(ApplicationDbContext context)
+    MatchesService _matchesService;
+    public MatchHub(ApplicationDbContext context, MatchesService matchesService)
     {
         _context = context;
+        _matchesService = matchesService;
     }
 
     public override async Task OnConnectedAsync()
     {
         await base.OnConnectedAsync();
+    }
+
+    public async Task JoinMatch(string userId, string connectionId, int specificMatchId)
+    {
+        await _matchesService.JoinMatch(userId, connectionId, specificMatchId);
+
+        string test = "Il se passe qqch.";
+        await Clients.User(userId).SendAsync("JoinMatch", test);
     }
 }
