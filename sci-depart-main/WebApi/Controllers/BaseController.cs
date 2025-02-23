8bf7bbd75b4f5307d5626ced2475ac3f476c0b75
Nazim <2378684@cegepmontpetit.ca>
2025-02-23 11:11:52 -0500

    FCT:register

--- sci-depart-main/WebApi/Controllers/BaseController.cs
+++ sci-depart-main/WebApi/Controllers/BaseController.cs
@@ -43,28 +43,40 @@ namespace Super_Cartes_Infinies.Controllers
             }
         }
         [HttpPost]
-        public async Task<ActionResult> Register(RegisterDTO register)
+        public async ActionResult Register(RegisterDTO register)
         {
             if (register.Password != register.PasswordConfirm)
             {
                 return StatusCode(StatusCodes.Status400BadRequest,
                     new { Message = "Les deux mots de passe spécifiés sont différents." });
             }
-    
-            
-            
 
+            IdentityUser user = new IdentityUser();
+            {
+                user.UserName = register.Username;
+                user.Email = register.Email;
+            }
             
-            
-               
+        IdentityResult identityResult = await playersService.CreatePlayer(user);
 
-                return Ok(new { Message = "Inscription réussie ! 🥳" });
+            if (!identityResult.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new { Message = "La création de l'utilisateur a échoué." });
             }
 
+
+
+
+
+            return Ok(new { Message = "Inscription réussie ! 🥳" });
         }
+    }
+
+    }
 
            
 
-    }
+    
 
 
