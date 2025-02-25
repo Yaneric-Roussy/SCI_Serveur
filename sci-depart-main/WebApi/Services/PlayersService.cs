5483756f15b0b5f2db5033d177988fdaa21e5f52
nazim <2378684@cegepmontpetit.ca>
2025-02-25 14:28:00 -0500

    FCT: faut terminer l'implémentation de la méthode CreatePlayer de PlayersService.

--- sci-depart-main/WebApi/Services/PlayersService.cs
+++ sci-depart-main/WebApi/Services/PlayersService.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Models.Models;
 using Super_Cartes_Infinies.Data;
 using Super_Cartes_Infinies.Models;
+using System.Numerics;
 
 namespace Super_Cartes_Infinies.Services
 {
 	public class PlayersService
     {
         private ApplicationDbContext _dbContext;
-        private readonly CardsService _startingCard;
+        private readonly StartingCardsService _startingCard;
 
         public PlayersService(ApplicationDbContext context,StartingCardsService startingcardService)
         {
@@ -24,16 +26,35 @@ namespace Super_Cartes_Infinies.Services
                 Name = user.Email!
             };
 
-       
+
             // TODO: Utilisez le service StartingCardsService pour obtenir les cartes de départ
             // TODO: Ajoutez ces cartes au joueur en utilisant le modèle OwnedCard que vous allez devoir ajouter
-            var starinCard = 
             _dbContext.Add(p);
             _dbContext.SaveChanges();
-            var startingCard = _startingCard.GetPlayersCards(user.Id);
+
+            var startingCard = _startingCard.GetStartingCards();
+            foreach (var card in startingCard)
+            {
+                var ownedCard = new OwnedCard() 
+                {
+                    Card = card,
+                    Player = p
+
+                }
+                ;
+                
+                
+              
+                _dbContext.Add(ownedCard); 
+           
+                _dbContext.SaveChanges();
+            }
 
             return p;
+         
+
         }
+
 
         public virtual Player GetPlayerFromUserId(string userId)
         {
