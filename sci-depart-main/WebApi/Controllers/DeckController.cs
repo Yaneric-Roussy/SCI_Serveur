91fa5a2fb4341225007db2ef5aa0582b1f908c64
nazim <2378684@cegepmontpetit.ca>
2025-04-02 14:53:49 -0400

    progress DeckService/Controller

--- sci-depart-main/WebApi/Controllers/DeckController.cs
+++ sci-depart-main/WebApi/Controllers/DeckController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Models.Models;
+using Super_Cartes_Infinies.Controllers;
+using Super_Cartes_Infinies.Data;
+using Super_Cartes_Infinies.Services;
+using WebApi.Services;
 
 namespace WebApi.Controllers
 {
-    public class DeckController : Controller
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class DeckController : ControllerBase
     {
-        public IActionResult Index()
+        private ApplicationDbContext _dbContext;
+        private CardsService _cardsService;
+        private DecksService _deckService;
+        [HttpGet]
+        public async Task<ActionResult> Adddeck(string name)
         {
-            return View();
+            _deckService.AjoutDeck(name);
+            return Ok();
         }
+        [HttpGet]
+        public async Task<ActionResult> GetDeck(string name)
+        {
+            _deckService.getDeck( name);
+            return Ok();
+        }
+
+        public DeckController(ApplicationDbContext dbContext, DecksService decksService)
+        {
+            _dbContext = dbContext;
+            _deckService = decksService;
+        }
+
     }
 }
--- sci-depart-main/WebApi/Services/DecksService.cs
+++ sci-depart-main/WebApi/Services/DecksService.cs
@@ -1,6 +1,35 @@
-﻿namespace WebApi.Services
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Models;
+using Super_Cartes_Infinies.Data;
+using Super_Cartes_Infinies.Services;
+using System.Collections;
+
+namespace WebApi.Services
 {
     public class DecksService
     {
+        private ApplicationDbContext _dbContext;
+
+        public DecksService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public async Task AjoutDeck(string name)
+        {
+            Deck newDeck = new Deck();
+            newDeck.Name = name;
+            await _dbContext.Decks.AddAsync(newDeck);
+            await _dbContext.SaveChangesAsync();
+        }
+        public IEnumerable<Deck> getDeck(string userId)
+        {
+            var player = _dbContext.Players.FirstOrDefault(p => p.UserId == userId);
+    
+                return _dbContext.Decks.Where(d => d.user.Id == userId).Include(s=>s.CarteJoueurs).ToList();
+           
+        }
+       
     }
+    
 }
