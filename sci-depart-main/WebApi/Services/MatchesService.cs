f2c7b4dcdaf2f6c22832863f4484d49b4c9e5183
nazim <2378684@cegepmontpetit.ca>
2025-04-14 10:34:37 -0400

    fct:Changer le démarrage d'un Match pour utiliser les cartes du deck courrant

--- sci-depart-main/WebApi/Services/MatchesService.cs
+++ sci-depart-main/WebApi/Services/MatchesService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Models.Models;
 using Super_Cartes_Infinies.Combat;
 using Super_Cartes_Infinies.Data;
 using Super_Cartes_Infinies.Models;
@@ -65,8 +66,9 @@ namespace Super_Cartes_Infinies.Services
                     playerB = _playersService.GetPlayerFromUserId(pairOfUsers.UserBId);
 
                     // Création d'un nouveau match
-                    IEnumerable<Card> cards = _cardsService.GetAllCards();
-                    match = new Match(playerA, playerB, cards);
+                    IEnumerable<Card> cardsPlayerA = await _cardsService.GetUserDeckCards(playerA.Id);
+                    IEnumerable<Card> cardsPlayerB = await _cardsService.GetUserDeckCards(playerB.Id);
+                    match = new Match(playerA, playerB, cardsPlayerA, cardsPlayerB);
                     otherPlayerConnectionId = pairOfUsers.UserAConnectionId;
 
                     _dbContext.Update(match);
@@ -111,7 +113,9 @@ namespace Super_Cartes_Infinies.Services
                 currentPlayerData = match.PlayerDataB;
                 opposingPlayerData = match.PlayerDataA;
             }
-            Player p1 = await _dbContext.Players.FirstOrDefaultAsync(p => p.Id == currentPlayerData.Id);
+            
+          
+           
             
      
             int nbCardsToDraw = _matchConfigurationService.GetNbCardsToDraw();
