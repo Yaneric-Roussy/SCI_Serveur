594020ada123a49d1a946949f372307de3f02a0c
nazim <2378684@cegepmontpetit.ca>
2025-05-02 13:46:00 -0400

    Ajout deChat controller

---
+++ sci-depart-main/WebApi/Hubs/Chat.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Super_Cartes_Infinies.Data;
+using Super_Cartes_Infinies.Models;
+using Super_Cartes_Infinies.Services;
+using System.Reflection;
+
+
+namespace WebApi.Hubs
+{
+    public class Chat: Hub
+    {
+        ApplicationDbContext _context;
+        MatchesService _matchesService;
+        IEnumerable<Match> Matches = new List<Match>();
+
+
+
+
+
+        public Chat(ApplicationDbContext context, MatchesService matchesService)
+        {
+            _context = context;
+            _matchesService = matchesService;
+        }
+        public override Task OnConnectedAsync()
+        {
+            // You can retrieve the user identifier from the context (e.g., from authentication)
+            var userId = Context.UserIdentifier; // Here you can set a custom UserIdentifier based on your app's logic.
+            return base.OnConnectedAsync();
+        }
+
+   
+        
+       public async Task UserPlaying()
+        {
+
+        }
+
+        public async Task UserWatching()
+        {
+
+        }
+
+        public async Task GetActiveMatches()
+        {
+               Matches = await _context.Matches.Where(m => m.IsMatchCompleted == false).ToListAsync();
+          
+            
+       
+        }
+    }
+}
