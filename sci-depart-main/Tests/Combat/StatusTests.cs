4dd71cb69b7df3dab1e57175909bf1df6c94fe6c
yaneric <2243398@cegepmontpetit.ca>
2025-05-07 17:15:56 -0400

    PROGRÈS : Ajouté des tests pour les fonctions gestionnaires
    Non implémenté.

---
+++ sci-depart-main/Tests/Combat/StatusTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Super_Cartes_Infinies.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Combat
+{
+    [TestClass]
+    public class StatusTests
+    {
+        private ApplicationDbContext _db;
+
+        [TestInitialize]
+        public void Init()
+        {
+            string dbName = "StatusTests" + Guid.NewGuid().ToString();
+            DbContextOptions<ApplicationDbContext> options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .UseLazyLoadingProxies(true) // Active le lazy loading
+                .Options;
+
+            _db = new ApplicationDbContext(options);
+        }
+
+        [TestMethod]
+        public void HasStatusValid()
+        {
+
+        }
+
+        [TestMethod]
+        public void HasStatusNotValid()
+        {
+
+        }
+
+        [TestMethod]
+        public void AddStatusValueValid()
+        {
+
+        }
+
+        [TestMethod]
+        public void AddStatusNotValid()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetStatusValueValid()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetStatusValue()
+        {
+
+        }
+    }
+}
