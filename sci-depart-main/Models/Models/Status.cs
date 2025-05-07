d870e0d62c494021784ff772a5e0df0fd9dcefb8
yaneric <2243398@cegepmontpetit.ca>
2025-05-07 15:47:18 -0400

    FCT : Ajouté la classe Status

---
+++ sci-depart-main/Models/Models/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icone { get; set; }
+    }
+}
