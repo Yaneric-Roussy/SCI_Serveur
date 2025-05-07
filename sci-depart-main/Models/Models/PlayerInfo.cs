da013d3bf89da94c90a32982332ec054497fc1a3
AnthoSta <2390005@cegepmontpetit.ca>
2025-05-07 17:06:20 -0400

    Ajout PlayerInfo qui marche pas (Besoin de fix svp help)

---
+++ sci-depart-main/Models/Models/PlayerInfo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class PlayerInfo
+    {
+        public PlayerInfo() { }
+
+        public int Id { get; set; }
+        public int Elo { get; set; }
+        public int Attente { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
