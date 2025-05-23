78fee4c363610fe61f661f5e85a3f0ebd2b02f4c
Yaneric <2243398@cegepmontpetit.ca>
2025-05-23 15:19:27 -0400

    BUGFIX : Changé logique de vérification de status pour poison
    Avant on enlevait 1 à la durée et ensuite on vérifiait la durée en faisant -1. Revenait à faire -2.

--- sci-depart-main/WebApi/Combat/PoisonedEvent.cs
+++ sci-depart-main/WebApi/Combat/PoisonedEvent.cs
@@ -12,28 +12,32 @@ namespace Super_Cartes_Infinies.Combat
         public override string EventType { get { return "PoisonedEvent"; } }
         public int PlayerId { get; set; }
         public int CardId { get; set; }
-        public List<PlayableCardStatus> PlayableCardStatus { get; set; }
+        public List<PlayableCardStatus> PlayableCardsStatus { get; set; }
         public PoisonedEvent(MatchPlayerData currentPlayerData, PlayableCard playerCard)
         {
             this.Events = new List<MatchEvent>();
-            int value = playerCard.GetStatusValue(Status.POISONED_ID);
 
-            if (value > 0)
+            int currentValue = playerCard.GetStatusValue(Status.POISONED_ID);
+
+            if (currentValue > 0)
             {
-                //On cause du dégât et on réduit de un la durée
-                this.Events.Add(new CardDamageEvent(currentPlayerData, value, playerCard));
+                this.Events.Add(new CardDamageEvent(currentPlayerData, currentValue, playerCard));
                 playerCard.AddStatusValue(Status.POISONED_ID, -1);
-            }
-            if ((value - 1) <= 0)
-            {
-                //On enlève le pcs parce que l'effet est fini.
-                var pcs = playerCard.PlayableCardsStatus.First(c => c.StatusId == Status.POISONED_ID);
-                playerCard.PlayableCardsStatus.Remove(pcs);
+                int updatedValue = playerCard.GetStatusValue(Status.POISONED_ID);
+                if (updatedValue <= 0)
+                {
+                    var pcs = playerCard.PlayableCardsStatus.FirstOrDefault(c => c.StatusId == Status.POISONED_ID);
+                    if (pcs != null)
+                    {
+                        playerCard.PlayableCardsStatus.Remove(pcs);
+                    }
+                }
             }
 
             PlayerId = currentPlayerData.PlayerId;
             CardId = playerCard.Id;
-            PlayableCardStatus = playerCard.PlayableCardsStatus;
+            PlayableCardsStatus = playerCard.PlayableCardsStatus;
         }
+
     }
 }
